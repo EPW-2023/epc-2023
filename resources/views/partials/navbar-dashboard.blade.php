3fc4650e3b3e7043afa7cb59e26a6cd37d7a8550
Haznul24 <abrarhaznul@gmail.com>
2023-02-08 17:03:15 +0700

    change navbar

--- resources/views/partials/navbar-dashboard.blade.php
+++ resources/views/partials/navbar-dashboard.blade.php
@@ -2,23 +2,25 @@
     <div class="container-fluid">
         <img src="{{ asset('img/Logo EPW.png') }}" width="60" alt="">
         <a class="navbar-brand ms-4" href="#">EPW 2023</a>
-        <button type="button" class="btn btn-primary bg-transparent border-0" id="button-modal" data-bs-toggle="modal"
-            data-bs-target="#exampleModal">
-            <i class="bi bi-list fs-3"></i>
+        <button type="button" class="btn btn-primary  bg-transparent border-0" id="button-modal" type="button"
+            data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars fs-3"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 @guest()
-                    <button type="submit" style="background-color: transparent; border: none">
-                        <a class="nav-link nav-link-epw" href="/login" id="nav-link-epw">LOGIN</a>
+                    <li class="nav-item text-center">
+                        <a class="nav-link nav-link-epw" id="nav-link-epw" href="/login">Login</a>
+                    </li>
                     @endguest
                     @auth()
                         <li class="nav-item">
                             <form action="{{ route('applicant-logout') }}" method="POST">
                                 @csrf
-                                <button type="submit" style="background-color: transparent; border: none">
-                                    <a class="nav-link nav-link-epw" id="nav-link-epw">LOGOUT</a>
-                                </button>
+                                <li class="nav-item text-center">
+                                    <a class="nav-link nav-link-epw" id="nav-link-epw" href="/login">Logout</a>
+                                </li>
                             </form>
                         </li>
                     @endauth
@@ -26,7 +28,7 @@
         </div>
     </div>
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <!--<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -34,13 +36,9 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body text-center">
-                    <a href="#" class="d-block text-decoration-none my-2 link-modal">Home</a>
-                    <a href="#" class="d-block text-decoration-none my-2 link-modal">About</a>
-                    <a href="#" class="d-block text-decoration-none my-2 link-modal">EPC</a>
-                    <a href="#" class="d-block text-decoration-none my-2 link-modal">INJECTION</a>
-                    <a href="#" class="d-block text-decoration-none my-2 link-modal">Merch</a>
+                    <a href="/login" class="d-block text-decoration-none my-2 link-modal">Login</a>
                 </div>
             </div>
         </div>
-    </div>
+    </div>-->
 </nav>
